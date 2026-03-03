97c5326f109a176aed6b48dff2b72b7c837e8276
Roaa Mostafa <roaa.m.elsayed@gmail.com>
2026-03-03 20:01:27 +0200

    heap sort

--- src/main/java/algorithms/HeapSort.java
+++ src/main/java/algorithms/HeapSort.java
@@ -16,4 +16,53 @@ public class HeapSort {
         this.arr = arr;
     }
 
+    public void sort() {
+        int n = arr.length;
+
+        // Build heap (rearrange array)
+        for (int i = n / 2 - 1; i >= 0; i--)
+            heapify(n, i);
+
+        // One by one extract elements from heap
+        for (int i = n - 1; i > 0; i--) {
+            // Move current root to end
+            int temp = arr[0];
+            arr[0] = arr[i];
+            arr[i] = temp;
+            interchanges++;
+
+            // call max heapify on the reduced heap
+            heapify(i, 0);
+        }
+    }
+
+    public  void heapify(int n, int i) {
+        int largest = i; // Initialize largest as root
+        int l = 2 * i + 1; // left = 2*i + 1
+        int r = 2 * i + 2; // right = 2*i + 2
+
+        // If left child is larger than root and node has children (node's index is less than heap size)
+        if (l < n && arr[l] > arr[largest]) {
+            comparisons++;
+            largest = l;
+        }
+
+        // If right child is larger than largest so far
+        if (r < n && arr[r] > arr[largest]) {
+            comparisons++;
+            largest = r;
+        }
+
+        // If largest is not root
+        if (largest != i) {
+            int swap = arr[i];
+            arr[i] = arr[largest];
+            arr[largest] = swap;
+            interchanges++;
+
+            // Recursively heapify the affected sub-tree
+            heapify(n, largest);
+        }
+    }
+
 }
